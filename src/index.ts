10f2c9fd1d68cbac8d67e34bd4680f36ca9a9ac1
windbullet <2536810643@qq.com>
2024-04-23 21:39:04 +0800

    提交文件

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,52 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, h } from 'koishi'
+import { } from 'koishi-plugin-ffmpeg'
+import { } from 'koishi-plugin-silk'
 
 export const name = 'audio-reverse'
 
-export interface Config {}
+export interface Config { }
 
 export const Config: Schema<Config> = Schema.object({})
 
+export const inject = ["ffmpeg", "silk"]
+
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.command("倒放语音")
+    .action(async ({ session }) => {
+      let elements
+      if (session.quote) {
+        elements = session.quote.elements
+      } else {
+        await session.send("在60秒内发送想要倒放的语音")
+        let msg = await session.prompt(60000)
+        if (msg !== undefined) {
+          elements = h.parse(msg)
+        }
+      }
+      let audio = h.select(elements, 'audio')
+      if (audio.length > 0) {
+        const res = await ctx.http(audio[0].attrs.src, { responseType: 'arraybuffer' })
+        const type = res.headers.get('content-type')
+        let input = Buffer.from(res.data)
+        if (session.platform === 'red') {
+          const pcm = await ctx.silk.decode(res.data, 24000)
+          const wav = await ctx.ffmpeg
+            .builder()
+            .input(Buffer.from(pcm.data))
+            .inputOption('-f', 's16le', '-ar', '24000', '-ac', '1')
+            .outputOption('-f', 'wav')
+            .run("buffer")
+          input = wav
+        }
+        const reversedAudio = await ctx.ffmpeg
+          .builder()
+          .input(input)
+          .outputOption('-af', 'areverse', '-f', 'wav')
+          .run("buffer")
+        if (reversedAudio.length === 0) return "音频倒放失败"
+        return h.audio(reversedAudio, type)
+      } else {
+        return "这看上去不是音频"
+      }
+    })
 }
