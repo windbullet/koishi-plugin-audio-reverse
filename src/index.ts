297adb086fede0715084871a36e0ecf135e2e8fb
idranme <idranme@outlook.com>
2024-04-24 16:52:10 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,12 @@ import { } from 'koishi-plugin-silk'
 export const name = 'audio-reverse'
 export const inject = ['ffmpeg', 'silk']
 
-export const using = '支持调用指令后发语音和回复语音两种方法'
-
 export interface Config { }
 
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  ctx.command('audio-reverse')
+  ctx.command('audio-reverse', '对语音进行倒放')
     .alias('倒放语音')
     .usage('支持调用指令后发语音和回复语音两种方法')
     .option('speed', '-s <speed:number> 设置倍速', { fallback: 1 })
@@ -31,26 +29,30 @@ export function apply(ctx: Context) {
       }
 
       const audio = h.select(elements, 'audio')
-      if (audio.length === 0) return '这看上去不是音频'
+      if (audio.length === 0) return '这看上去不是音频。'
 
       const res = await ctx.http(audio[0].attrs.src, { responseType: 'arraybuffer' })
-      let input = Buffer.from(res.data)
-      if (session.platform === 'red') {
+      if (!ctx.silk.isSilk) throw new Error('请更新 silk 插件至最新版本')
+
+      let reversedAudio: Buffer
+      const outputOption = ['-f', 'wav', '-af', `areverse,atempo=${options.speed}`]
+      if (ctx.silk.isSilk(res.data)) {
         const pcm = await ctx.silk.decode(res.data, 24000)
-        input = await ctx.ffmpeg
+        reversedAudio = await ctx.ffmpeg
           .builder()
           .input(Buffer.from(pcm.data))
           .inputOption('-f', 's16le', '-ar', '24000', '-ac', '1')
-          .outputOption('-f', 'wav')
+          .outputOption(...outputOption)
+          .run('buffer')
+      } else {
+        reversedAudio = await ctx.ffmpeg
+          .builder()
+          .input(Buffer.from(res.data))
+          .outputOption(...outputOption)
           .run('buffer')
       }
-
-      const reversedAudio = await ctx.ffmpeg
-        .builder()
-        .input(input)
-        .outputOption('-f', 'wav', '-af', `areverse,atempo=${options.speed}`)
-        .run('buffer')
-      if (reversedAudio.length === 0) return '音频倒放失败'
+      if (reversedAudio.length === 0) return '音频倒放失败。'
+      
       return h.audio(reversedAudio, 'audio/vnd.wave')
     })
 }
